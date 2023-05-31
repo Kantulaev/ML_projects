bcba80080ba57804ea23a3c6302170ebedbd4bf0
Kantulaev <Kantulaev@gmail.com>
2023-05-31 20:35:38 +0600

    parsing

--- my_projects/web_scraping/lesson_3/lesson_3_web_scraping_pages.py
+++ my_projects/web_scraping/lesson_3/lesson_3_web_scraping_pages.py
@@ -12,17 +12,18 @@ for page in range(1, 2):
     for h in hrefs:
         links.append((h['href'][6:-11]))
     linksnorep = list(dict.fromkeys(links))
+
     for link in linksnorep:
         try:
             result = requests.get(f'{root}catalogue/{link}/index.html')
             content = result.text
             soup = BeautifulSoup(content, 'lxml')
             main_article = soup.find('article', class_="product_page")
-            title = main_article.find('h1').get_text(strip=True)
+            title = main_article.find('h1').get_text()
             available = main_article.find_all('tr')[5].findNext().findNext().get_text()
             description = main_article.find('div', id='product_description').findNextSibling().get_text(strip=True)
 
-            with open(f'lesson_3_movies/{title}.txt', 'w', encoding='utf-8') as file:
+            with open(f'lesson_3_movies/{title.replace(":", "")}.txt', 'w', encoding='utf-8') as file:
                 file.write(available + '\n\n' + description)
 
         except AttributeError:
